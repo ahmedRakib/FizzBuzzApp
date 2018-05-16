03d2ac8d3eaac05650fed860a8d8d84e24a2ba98
hrakibul <hrakibul@aexcellences.com>
2018-05-17 02:07:12 +0600

    all the files added

---
+++ FizzBuzzApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FizzBuzzApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Press a Number");
+
+            int aNumber;
+
+            if (int.TryParse(Console.ReadLine(), out aNumber))
+            {
+                if (aNumber % 3 == 0 && aNumber % 5 == 0)
+                {
+                    Console.WriteLine("FiZZBuZZ");
+                }
+
+                else if (aNumber % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (aNumber % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine("Not A FiZZ or Buzz Word. Not Even FizzBuzz ");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Man!!! I asked to enter a number. -_- ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
